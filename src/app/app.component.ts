d9840a3d465987d536081af03fb55be813d5dd55
i345275 <estelle.de.magondeaux@sap.com>
2021-06-04 12:59:08 +0200

    added session time, karma conf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,15 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {BookService} from './services/book.service';
-
-import {error} from "selenium-webdriver";
-import {Observable, Subscription} from 'rxjs';
-import { interval } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit{
   title = 'sap-fe-interviews';
-  ok: boolean;
-  secondes: number;
-  voteEnded: boolean;
-  counterSubscription: Subscription;
+
   constructor() {
-    this.ok = true;
   }
   ngOnInit(): void {
-    this.voteEnded = false;
-    const counter = interval(1000);
-    this.counterSubscription = counter.subscribe(
-      (value) => {
-        this.secondes = value;
-      },
-      (error) => {
-        console.log('Uh-oh, an error occurred! : ' + error);
-      },
-      () => {
-        console.log('Observable complete!');
-      }
-    );
-  }
-  ngOnDestroy() {
-    this.counterSubscription.unsubscribe();
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  sessionTime: number;
+  voteEnded: boolean;
+  counterSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.voteEnded = false;
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.sessionTime = value;
+      },
+      (error) => {
+        console.log('an error occurred when counting : ' + error);
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
   }
 
 }
